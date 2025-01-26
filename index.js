3d91bdfc0a1e08cdc5f667fcc8a1b14b63ca0127
Gang Luo <niyhyhiy@gmail.com>
2025-01-26 16:38:23 -0500

    add all the files except readme.

---
+++ index.js
@@ -0,0 +1,112 @@
+const characters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '~',
+  '`',
+  '!',
+  '@',
+  '#',
+  '$',
+  '%',
+  '^',
+  '&',
+  '*',
+  '(',
+  ')',
+  '_',
+  '-',
+  '+',
+  '=',
+  '{',
+  '[',
+  '}',
+  ']',
+  ',',
+  '|',
+  ':',
+  ';',
+  '<',
+  '>',
+  '.',
+  '?',
+  '/',
+]
+
+let $passwordOne = document.getElementById('passwordOne')
+let $passwordTwo = document.getElementById('passwordTwo')
+
+function generatePassword() {
+  let password = ''
+  for (let i = 0; i < 15; i++) {
+    password += characters[Math.floor(Math.random() * characters.length)]
+  }
+  return password
+}
+
+function renderPasswords() {
+  let passwordOne = generatePassword()
+  let passwordTwo = generatePassword()
+
+  $passwordOne.textContent = passwordOne
+  $passwordTwo.textContent = passwordTwo
+}
